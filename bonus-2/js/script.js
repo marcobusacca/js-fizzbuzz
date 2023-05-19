26e82560139353dd49debcf8d5da7272a272555d
marcobusacca <marcobusacca01@gmail.com>
2023-05-19 16:08:47 +0200

    [BONUS-2] inizializzazione cartella bonus-2

---
+++ bonus-2/js/script.js
@@ -0,0 +1,36 @@
+// CICLO FOR CHE STAMPA NUMERI DA 1 A 100
+for(let i= 1; i<=100; i++){
+
+    // DICHIARAZIONE E INIZIALIZZAZIONE CONSTANTE CON TAG DIV, DA INSERIRE ALL'INTERNO CONSTANTE "OUTPUT"
+    const output_number = document.createElement('div');
+
+    // DICHIARAZIONE E INIZIALIZZAZIONE CONSTANTE RIFERITA AL TAG HTML CON ID "OUTPUT"
+    const output = document.getElementById('output');
+
+    // INSERIMENTO CONTENUTO CONSTANTE "OUTPUT_NUMBER" ALL'INTERNO DELLA COSTANTE "OUTPUT"
+    output.append(output_number);
+
+    // CONTROLLO CHE IL NUMERO STAMPATO È MULTIPLO SIA DI 3 CHE DI 5
+    if (i % 3 === 0 && i % 5 === 0){
+        let number = "FizzBuzz";
+        output_number.append(number);
+    }
+
+    // CONTROLLO CHE IL NUMERO STAMPATO È MULTIPLO DI 3
+    else if (i % 3 === 0){
+        let number = "Fizz";
+        output_number.append(number);
+    } 
+
+    // CONTROLLO CHE IL NUMERO STAMPATO È MULTIPLO DI 5
+    else if (i % 5 === 0){
+        let number = "Buzz";
+        output_number.append(number);
+    } 
+    
+    // IL NUMERO STAMPATO NON È MULTIPLO NÈ DI 3, NÈ DI 5, NÈ DI ENTRAMBI, QUINDI STAMPO IL NUMERO
+    else{
+        let number = i;
+        output_number.append(number);
+    }
+}
---
+++ bonus-2/readme.md
@@ -0,0 +1,29 @@
+PROBLEMA : Scrivi un programma che stampi in Console i numeri da 1 a 100. 
+
+    - Per i numeri che sono multipli sia di 3, sia di 5, stampa “FizzBuzz” al posto del numero.
+
+    - Per i multipli di 3 stampa “Fizz” al posto del numero.
+
+    - Per i multipli di 5 stampa “Buzz” al posto del numero.
+
+
+
+1 - Crea un Ciclo FOR che stampi i numeri da 1 a 100
+
+    1.1 - ? SE il numero stampato è multiplo sia di 3, sia di 5, ALLORA
+
+        1.1.1 - Al posto del numero, stampa “FizzBuzz”;
+
+        1.1.2 - Stampa il risultato nel DOM;
+
+    1.2 - ALTRIMENTI SE il numero stampato è multiplo soltanto di 3, ALLORA
+
+        1.2.1 - Al posto del numero, stampa “Fizz”;
+
+        1.2.2 - Stampa il risultato nel DOM;
+
+    1.3 - ALTRIMENTI SE il numero stampato è multiplo soltanto di 5, ALLORA
+
+        1.3.1 - Al posto del numero, stampa “Buzz”;
+
+        1.3.2 - Stampa il risultato nel DOM;
