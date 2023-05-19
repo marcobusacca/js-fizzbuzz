196df3c7a031511f3bc45a1af6b87f740e426295
marcobusacca <marcobusacca01@gmail.com>
2023-05-19 15:35:31 +0200

    [BONUS-1] Inserimento elementi nel DOM HTML

--- bonus-1/js/script.js
+++ bonus-1/js/script.js
@@ -1,23 +1,38 @@
 // CICLO FOR CHE STAMPA NUMERI DA 1 A 100
 for(let i= 1; i<=100; i++){
 
+    // DICHIARAZIONE E INIZIALIZZAZIONE CONSTANTE CON TAG DIV, DA INSERIRE ALL'INTERNO CONSTANTE "OUTPUT"
+    const output_number = document.createElement('div');
+
     // CONTROLLO CHE IL NUMERO STAMPATO È MULTIPLO SIA DI 3 CHE DI 5
     if (i % 3 === 0 && i % 5 === 0){
-        console.log("FizzBuzz");
+        let number = "FizzBuzz";
+        output_number.append(number);
     }
 
     // CONTROLLO CHE IL NUMERO STAMPATO È MULTIPLO DI 3
     else if (i % 3 === 0){
-        console.log("Fizz");
+        let number = "Fizz";
+        output_number.append(number);
     } 
 
     // CONTROLLO CHE IL NUMERO STAMPATO È MULTIPLO DI 5
     else if (i % 5 === 0){
-        console.log("Buzz");
+        let number = "Buzz";
+        output_number.append(number);
     } 
     
     // IL NUMERO STAMPATO NON È MULTIPLO NÈ DI 3, NÈ DI 5, NÈ DI ENTRAMBI, QUINDI STAMPO IL NUMERO
     else{
-        console.log(i);
+        let number = i;
+        output_number.append(number);
     }
+
+    // DICHIARAZIONE E INIZIALIZZAZIONE CONSTANTE RIFERITA AL TAG HTML CON ID "OUTPUT"
+    const output = document.getElementById('output')
+
+    // INSERIMENTO CONTENUTO CONSTANTE "OUTPUT_NUMBER" ALL'INTERNO DELLA COSTANTE "OUTPUT"
+    output.append(output_number)
 }
+// STAMPIAMO NEL TAG HTML CON ID "OUTPUT" IL CONTENUTO DELLA VARIABILE OUTPUT NUMBER
+
