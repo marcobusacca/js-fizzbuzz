883321e726be9a9383d8493c4d9ecee889ad596b
marcobusacca <marcobusacca01@gmail.com>
2023-05-19 15:38:35 +0200

    [BONUS-1] ordinamento js

--- bonus-1/js/script.js
+++ bonus-1/js/script.js
@@ -4,6 +4,12 @@ for(let i= 1; i<=100; i++){
     // DICHIARAZIONE E INIZIALIZZAZIONE CONSTANTE CON TAG DIV, DA INSERIRE ALL'INTERNO CONSTANTE "OUTPUT"
     const output_number = document.createElement('div');
 
+    // DICHIARAZIONE E INIZIALIZZAZIONE CONSTANTE RIFERITA AL TAG HTML CON ID "OUTPUT"
+    const output = document.getElementById('output')
+
+    // INSERIMENTO CONTENUTO CONSTANTE "OUTPUT_NUMBER" ALL'INTERNO DELLA COSTANTE "OUTPUT"
+    output.append(output_number)
+
     // CONTROLLO CHE IL NUMERO STAMPATO È MULTIPLO SIA DI 3 CHE DI 5
     if (i % 3 === 0 && i % 5 === 0){
         let number = "FizzBuzz";
@@ -27,12 +33,6 @@ for(let i= 1; i<=100; i++){
         let number = i;
         output_number.append(number);
     }
-
-    // DICHIARAZIONE E INIZIALIZZAZIONE CONSTANTE RIFERITA AL TAG HTML CON ID "OUTPUT"
-    const output = document.getElementById('output')
-
-    // INSERIMENTO CONTENUTO CONSTANTE "OUTPUT_NUMBER" ALL'INTERNO DELLA COSTANTE "OUTPUT"
-    output.append(output_number)
 }
 // STAMPIAMO NEL TAG HTML CON ID "OUTPUT" IL CONTENUTO DELLA VARIABILE OUTPUT NUMBER
 
