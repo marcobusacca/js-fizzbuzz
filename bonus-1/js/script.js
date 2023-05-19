0f5af3aafc00ea17a3b8c524518e4ccc3a25668c
marcobusacca <marcobusacca01@gmail.com>
2023-05-19 15:58:33 +0200

    Bonus-1 Completato

--- bonus-1/js/script.js
+++ bonus-1/js/script.js
@@ -5,10 +5,10 @@ for(let i= 1; i<=100; i++){
     const output_number = document.createElement('div');
 
     // DICHIARAZIONE E INIZIALIZZAZIONE CONSTANTE RIFERITA AL TAG HTML CON ID "OUTPUT"
-    const output = document.getElementById('output')
+    const output = document.getElementById('output');
 
     // INSERIMENTO CONTENUTO CONSTANTE "OUTPUT_NUMBER" ALL'INTERNO DELLA COSTANTE "OUTPUT"
-    output.append(output_number)
+    output.append(output_number);
 
     // CONTROLLO CHE IL NUMERO STAMPATO È MULTIPLO SIA DI 3 CHE DI 5
     if (i % 3 === 0 && i % 5 === 0){
