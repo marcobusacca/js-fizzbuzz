c597470aaab3d8a47f31db12a5c5be2440150f3f
marcobusacca <marcobusacca01@gmail.com>
2023-05-21 11:14:51 +0200

    [FIX] bonus-1 js, bonus-2 js

--- bonus-1/js/script.js
+++ bonus-1/js/script.js
@@ -1,15 +1,12 @@
 "use strict";
 // CICLO FOR CHE STAMPA NUMERI DA 1 A 100
-for(let i= 1; i<=100; i++){
-
-    // DICHIARAZIONE E INIZIALIZZAZIONE CONSTANTE CON TAG DIV, DA INSERIRE ALL'INTERNO CONSTANTE "OUTPUT"
-    const output_number = document.createElement('div');
+for(let i = 1; i <= 100; i++){
 
     // DICHIARAZIONE E INIZIALIZZAZIONE CONSTANTE RIFERITA AL TAG HTML CON ID "OUTPUT"
     const output = document.getElementById('output');
 
-    // INSERIMENTO CONTENUTO CONSTANTE "OUTPUT_NUMBER" ALL'INTERNO DELLA COSTANTE "OUTPUT"
-    output.append(output_number);
+    // DICHIARAZIONE E INIZIALIZZAZIONE CONSTANTE CON TAG DIV, DA INSERIRE ALL'INTERNO CONSTANTE "OUTPUT"
+    const output_number = document.createElement('div');
 
     // CONTROLLO CHE IL NUMERO STAMPATO È MULTIPLO SIA DI 3 CHE DI 5
     if (i % 3 === 0 && i % 5 === 0){
@@ -34,4 +31,7 @@ for(let i= 1; i<=100; i++){
         let number = i;
         output_number.append(number);
     }
+
+    // INSERIMENTO CONTENUTO CONSTANTE "OUTPUT_NUMBER" ALL'INTERNO DELLA COSTANTE "OUTPUT"
+    output.append(output_number);
 }
